c34ea029d549c5f36bf1783eb74038199631b90a
Zhouyuan Li <lizy@cs.wisc.edu>
2012-12-09 09:21:46 -0600

    ambient light added

--- scripts/game.js
+++ scripts/game.js
@@ -167,7 +167,7 @@ function Game(renderer, canvas) {
 
         //this.scene.add(new THREE.AmbientLight(0xffffff));
         //this.scene.add(new THREE.AmbientLight(0x06080e));
-        //this.scene.add(new THREE.AmbientLight(0x272727));
+        this.scene.add(new THREE.AmbientLight(0x0f0f0f));
 
         // Load the level
         this.level = new Level(this);
